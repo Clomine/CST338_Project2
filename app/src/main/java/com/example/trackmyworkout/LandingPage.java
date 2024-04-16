aa9e980a9b5da5fc369a030056f1a82509e869bb
Arthur Barreau <barreau.arthur@gmail.com>
2024-04-16 16:31:41 -0700

    Bottom Nav Bar Finished for every Activitys

--- app/src/main/java/com/example/trackmyworkout/LandingPage.java
+++ app/src/main/java/com/example/trackmyworkout/LandingPage.java
@@ -1,14 +1,48 @@
 package com.example.trackmyworkout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class LandingPage extends AppCompatActivity {
+
+import com.example.trackmyworkout.databinding.ActivityLandingPageBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LandingPage extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+
+        // The following part is for the Bottom Navigation Bar
+        ActivityLandingPageBinding binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView bottomNavigationView = binding.bottomNavigation;
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                int temp = item.getItemId();
+                if (R.id.home == temp) {
+                    intent = new Intent(LandingPage.this, LandingPage.class);
+                    startActivity(intent);
+                } else if (R.id.workout == temp){
+                    intent = new Intent(LandingPage.this, WorkoutPage.class);
+                    startActivity(intent);
+                } else if (R.id.converter == temp) {
+                    intent = new Intent(LandingPage.this, ConversionPage.class);
+                    startActivity(intent);
+                } else if (R.id.settings == temp) {
+                    intent = new Intent(LandingPage.this, SettingsPage.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+        // The above part is for the Bottom Navigation Bar
     }
 }
