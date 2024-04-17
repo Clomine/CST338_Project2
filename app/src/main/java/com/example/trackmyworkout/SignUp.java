66a75ea4968e7ba387509551998a5d5b3d3a1bdc
Clomine <clomineytb@gmail.com>
2024-04-16 17:04:02 -0700

    Sign Up UI/UX finished

--- app/src/main/java/com/example/trackmyworkout/SignUp.java
+++ app/src/main/java/com/example/trackmyworkout/SignUp.java
@@ -2,7 +2,10 @@ package com.example.trackmyworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ImageButton signInButton = findViewById(R.id.imageButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
